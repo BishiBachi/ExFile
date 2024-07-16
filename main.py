6cc453949f92672376b882ad783e8bb21b4703a1
JJaileiaY <jaileiayang@gmail.com>
2024-07-16 13:37:15 -0500

    Made the UI file connected to the py file so it pops up like an app by adding UI(QMainWindow) Class and initializing the app. Also fixed some spelling errors on the UI file.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# imports
+# Imports
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import sys
+
+from PyQt5.QtWidgets import QWidget
 
 def main():
     pass
@@ -8,3 +12,15 @@ if __name__ == '__main__':
     main()
 
 
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()    
+        
+        #Load and show the UI
+        uic.loadUi("exFile.ui", self)
+        self.show()
+
+#Initialize the app
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec()
